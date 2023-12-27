dc0eaf3798be10970649c80e615aeb9cfd9a6906
Chaoya Li <harry75369@gmail.com>
2023-12-27 12:53:17 +0800

    prepare for github page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "verygoodgraphics", // Usually your GitHub org/user name.
   projectName: "vgg_docs", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
