a15d708273d659a69dfea35ded8be2b05de174bf
MaybeMonad <dezineleo@gmail.com>
2024-02-04 18:55:51 +0800

    update: tweaks for v4.11

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,14 +39,13 @@ const config = {
   staticDirectories: ["static"],
 
   plugins: [
-    // ....
-    async function myPlugin(context, options) {
+    function myPlugin(context, options) {
       return {
         name: "docusaurus-tailwindcss",
         configurePostCss(postcssOptions) {
           // Appends TailwindCSS and AutoPrefixer.
           postcssOptions.plugins.push(require("tailwindcss"))
-          postcssOptions.plugins.push(require("autoprefixer"))
+          // postcssOptions.plugins.push(require("autoprefixer"))
           return postcssOptions
         },
       }
