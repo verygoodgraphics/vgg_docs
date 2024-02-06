651abe27297e1ecc35bed654b0efd4089049b1de
elf <elff1@outlook.com>
2024-02-06 17:45:37 +0800

    add "collapse sidebar" button; rename feature folders;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,6 +124,11 @@ const config = {
           },
         ],
       },
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       footer: {
         style: "dark",
         links: [
